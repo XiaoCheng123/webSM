cf1df1e437c0d4b8d4ef73c5296d7befc0567e6f
XiaoCheng123 <1197138309@qq.com>
2018-01-09 22:20:38 +0800

    连接数据库

---
+++ settings.js
@@ -0,0 +1,13 @@
+/**
+ * Created by admin on 2018/1/9.
+ */
+// 数据库连接基本信息
+module.exports = {
+  mysql: {
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'test'
+  }
+}
