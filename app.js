bdbb6febb2eaf1a7e1bee7bee753c348ca5601c3
XiaoCheng123 <1197138309@qq.com>
2018-01-09 20:39:46 +0800

    express初始化

---
+++ app.js
@@ -0,0 +1,32 @@
+/**
+ * Created by admin on 2018/1/9.
+ */
+var express = require('express');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/',function (req, res) {
+  res.type('txet/plain');
+  res.send('首页');
+})
+
+// 定制 404 页面
+app.use(function(req, res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+// 定制 500 页面
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('text/plain');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Express started on http://localhost:' +     app.get('port') + '; press Ctrl-C to terminate.')
+});
