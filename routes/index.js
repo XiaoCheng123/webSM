3f4212bb2a47a9500ca0e3109a7a7b6cb9a7cf0a
XiaoCheng123 <1197138309@qq.com>
2018-01-15 00:34:49 +0800

    添加增加学生

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('../controller/mysql.js')
+
 var searchStudent = require('../controller/search-student')
 var addStudent = require('../controller/add-student')
 var deleteStudent = require('../controller/delete-student')
+
 var searchGrade = require('../controller/searchGrade')
+var addGrade = require('../controller/add-grade')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -22,5 +25,7 @@ router.post('/deleteStudent', deleteStudent.get_click)
 // router.get('/addStudent', addStudent.get_click);
 
 router.get('/searchGrade', searchGrade.get_click);
+
+router.post('/addGrade', addGrade.get_click);
 
 module.exports = router;
