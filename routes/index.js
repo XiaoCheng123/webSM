475a1d5e85866095b9bcf0cf4026099ddb183f42
XiaoCheng123 <1197138309@qq.com>
2018-01-15 00:46:52 +0800

    添加删除成绩功能

--- routes/index.js
+++ routes/index.js
@@ -8,24 +8,22 @@ var deleteStudent = require('../controller/delete-student')
 
 var searchGrade = require('../controller/searchGrade')
 var addGrade = require('../controller/add-grade')
+var deleteGrade = require('../controller/delete-grade')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/data', mysql.get_click);
-
-router.get('/searchStudent', searchStudent.get_click);
-
-router.post('/addStudent', addStudent.get_click);
-
+router.get('/data', mysql.get_click)
+router.get('/searchStudent', searchStudent.get_click)
+router.post('/addStudent', addStudent.get_click)
 router.post('/deleteStudent', deleteStudent.get_click)
 
 // router.get('/addStudent', addStudent.get_click);
 
-router.get('/searchGrade', searchGrade.get_click);
-
-router.post('/addGrade', addGrade.get_click);
+router.get('/searchGrade', searchGrade.get_click)
+router.post('/addGrade', addGrade.get_click)
+router.post('/deleteGrade', deleteGrade.get_click)
 
 module.exports = router;
