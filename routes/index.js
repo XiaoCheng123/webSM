68af38bff7b5fa1fb1452a79f4fcf438fd85d5dc
XiaoCheng123 <1197138309@qq.com>
2018-01-15 01:36:41 +0800

    添加修改学生信息路由

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ var mysql = require('../controller/mysql.js')
 var searchStudent = require('../controller/search-student')
 var addStudent = require('../controller/add-student')
 var deleteStudent = require('../controller/delete-student')
+var reviseStudent = require('../controller/revise-student')
 
 var searchGrade = require('../controller/searchGrade')
 var addGrade = require('../controller/add-grade')
@@ -19,6 +20,7 @@ router.get('/data', mysql.get_click)
 router.get('/searchStudent', searchStudent.get_click)
 router.post('/addStudent', addStudent.get_click)
 router.post('/deleteStudent', deleteStudent.get_click)
+router.post('/reviseStudent', reviseStudent.get_click)
 
 // router.get('/addStudent', addStudent.get_click);
 
